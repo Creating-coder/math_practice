bfdebd22a1db5a0662424640ac4c7197c6b83c25
Pascal Qin <pascal.qin@gmail.com>
2019-04-04 10:46:24 -0700

    Simplify

--- ?.py
+++ ?.py
@@ -10,13 +10,26 @@ if __name__ == "__main__":
     total_t = 0
     total_rel = 0
     while True:
-        x = random.randint(100,999)
-        y = random.randint(10000,99999)
-        operation_decider = random.randint(0,1)
-        if operation_decider == 1:
-            z = x*y
+        question_decider = random.randint(0,1)
+        if question_decider == 1:
+            operation_decider = random.randint(0, 1)
+            if operation_decider == 1:
+                x = random.randint(100000, 999999)
+            else:
+                x = random.randint(1000000, 9999999)
+            z = pow(x, 1 / 2)
         else:
-            z = y/x
+            y = random.randint(10000,99999)
+            operation_decider = random.randint(0, 2)
+            if operation_decider == 0:
+                x = random.randint(100, 999)
+                z = x*y
+            elif operation_decider == 2:
+                x = random.randint(10, 99)
+                z = y / x
+            else:
+                x = random.randint(100, 999)
+                z = y/x
         time_t = 0
         rel = 0
         a = 0.95 * z
@@ -25,10 +38,13 @@ if __name__ == "__main__":
         estimation = 0
         t = time.time()
         while forever:
-            if operation_decider == 1:
-                print(f'Estimate the product of {x} and {y}')
+            if question_decider == 1:
+                print(f'Estimate the square root of {x}')
             else:
-                print(f'Estimate {y} divided by {x}')
+                if operation_decider == 0:
+                    print(f'Estimate the product of {x} and {y}')
+                else:
+                    print(f'Estimate {y} divided by {x}')
             estimation = input()
             time_t = time.time() - t
             if estimation == "exit":
@@ -42,10 +58,10 @@ if __name__ == "__main__":
                 continue
             questions += 1
             total_t += time_t
-            rel = math.fabs(100 - 100 * estimation / z)
+            rel = math.fabs(100 - 100 * float(estimation) / float(z))
             total_rel += rel
             forever = False
-        if estimation >= a and estimation <= b:
+        if int(a) <= estimation <= int(b):
             print(f'Good estimation! It took you {time_t} seconds. You answered between {int(a)} and {int(b)}! Your relative error is {int(rel)}%.')
             score += 1
         else:
